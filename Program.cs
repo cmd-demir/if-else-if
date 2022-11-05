249086131c87a6200437cd3e628dfdeeef8ff201
Cem Demir <cmd1220@gmail.com>
2022-11-05 14:24:43 +0300

    değişti

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+
+        if (time>= 12 && time<= 15)
+        Console.WriteLine("Öğlen!");
+        
+        else if(time<= 18)
+        Console.WriteLine("İyi Günler!");
+        else
+        Console.WriteLine("İyi Geceler!");
+
+        string sonuc = time<= 18 ? "İyi Günler" : "İyi Geceler";
+
+        sonuc = time<= 15 ? "Öğlen": time<= 18 ? "İyi Günler": "İyi Geceler";
+
+        Console.WriteLine(sonuc);
+
+        
+        
+
+                
+    
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
